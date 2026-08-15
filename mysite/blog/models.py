a9444895af7d3f68eb0dfe993b5db5561d535856
Douglas Dias <douglas.dias2102@outlook.com>
2026-08-15 16:58:31 -0300

    Add relationship between Post and User

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
@@ -8,7 +9,12 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     # 'Slug' is a short label ("Learning Python: So fun" --> "learning-python-so-fun")
-    slug = models.SlugField(max_length=250)     
+    slug = models.SlugField(max_length=250)    
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,   # Provided by Django
+        on_delete=models.CASCADE,   # on delete an User, delete its Posts
+        related_name='blog_posts'   # reverse relationship, from User to Post
+    ) 
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
