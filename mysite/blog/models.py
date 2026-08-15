0f9fb86f08d355c0ba012edeaac578124100744e
Douglas Dias <douglas.dias2102@outlook.com>
2026-08-15 15:59:01 -0300

    Add 'status' property to Post

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
+    class Status(models.TextChoices):   # Kinda like an'Enum'
+        DRAFT = 'DF', 'Draft'           # 'DF' is the value, 'Draft' is the readable name (label)
+        PUBLISHED = 'PB', 'Published'   # 'PB' is the value, 'Published' is the readable name (label)
+
     title = models.CharField(max_length=250)
     # 'Slug' is a short label ("Learning Python: So fun" --> "learning-python-so-fun")
     slug = models.SlugField(max_length=250)     
@@ -9,6 +13,11 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
 
     # This class defines metadata for the model
     class Meta:
